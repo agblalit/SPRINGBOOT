e5e1868cad4c787c04dd57d943421f09dd60a6a2
agblalit <agblalit@rediffmail.com>
2018-03-23 07:44:04 +0000

    first commit

---
+++ src/main/java/org/lalit/dev/DevApplication.java
@@ -0,0 +1,13 @@
+package org.lalit.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello DevApplication");
+		SpringApplication.run(DevApplication.class, args);
+	}
+}
